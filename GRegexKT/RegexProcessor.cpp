6bea8543eb78c9cfcbdb87912ce2b0e1180828f7
Gizak <zaknafein.dourden.2012@gmail.com>
2012-08-21 00:32:34 +0800

    add C++ processor

---
+++ GRegexKT/RegexProcessor.cpp
@@ -0,0 +1,9 @@
+//
+//  RegexProcessor.cpp
+//  GRegexKT
+//
+//  Created by Do'Urden Gizak on 12-8-21.
+//  Copyright (c) 2012年 Do'Urden Gizak. All rights reserved.
+//
+
+#include "RegexProcessor.h"
---
+++ GRegexKT/RegexProcessor.h
@@ -0,0 +1,14 @@
+//
+//  RegexProcessor.h
+//  GRegexKT
+//
+//  Created by Do'Urden Gizak on 12-8-21.
+//  Copyright (c) 2012年 Do'Urden Gizak. All rights reserved.
+//
+
+#ifndef __GRegexKT__RegexProcessor__
+#define __GRegexKT__RegexProcessor__
+
+#include <iostream>
+
+#endif /* defined(__GRegexKT__RegexProcessor__) */
